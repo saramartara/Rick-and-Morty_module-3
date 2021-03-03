fadfddbf10ef2039ef3d1b42233b04dd99dbaf82
saramartara <saramartinezaragon@gmail.com>
2021-03-03 21:38:09 +0100

    update CharacterCard component with destructuring code

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const CharacterCard = (props) => {
   console.log('estoy en tarjeta:', props.character)
-  // const { name, species, image } = props.characters;
+  const { name, species, image } = props.character;
   return (
     <>
       <img
-        alt={props.character.name}
-        src={props.character.image}
+        alt={name}
+        src={image}
       />
       <div className="bk container">
-        <h4 className="bk name">{props.character.name}</h4>
-        <p className="bk species">{props.character.species}</p>
+        <h4 className="bk name">{name}</h4>
+        <p className="bk species">{species}</p>
       </div>
    </>
   );
