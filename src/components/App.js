e0e2609eb2112d7585a940846ae9b8f4c30b0745
saramartara <saramartinezaragon@gmail.com>
2021-03-03 17:39:39 +0100

    add first html structure and basic styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import '../stylesheets/App.scss';
-import { Link, Route, Switch } from 'react-router-dom';
-import List from './List';
+// import { Link, Route, Switch } from 'react-router-dom';
+// import List from './List';
 
 function App(props) {
   return (
     <>
-      <div className="App">
-        <h1 className="title">Evaluación Final!</h1>
-        <button className="btn">clícame</button>
-        {/* <Link to="/list">List</Link> */}
-      </div>
-      <Switch>
-        {/* <Route exact path="/" component={Home} /> */}
-        <Route path="/list" component={List} />
-      </Switch>
+      <h1>
+        {/* <img
+          src="https://fontmeme.com/permalink/210303/89204f57cba876638717858b006d3c8f.png"
+          alt="fuente-get-schwifty"
+          border="0"
+        /> */}
+
+        <img
+          src="https://fontmeme.com/permalink/210303/18269efaf0589bdc79aaf3cea37ab67b.png"
+          alt="fuente-get-schwifty"
+          border="0"
+        />
+      </h1>
+      {/* 
+      <h1 className="title">Rick and Morty</h1> */}
+      <form>
+        <input></input>
+      </form>
+
+      <section>
+        <ul className="listContainer">
+          <li className="list">
+            <img
+              alt="Rick Sanchez"
+              src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            />
+            <div className="bk container">
+              <h4 className="bk name">Rick Sanchez</h4>
+              <p className="bk species">Human</p>
+            </div>
+          </li>
+          {/* <li className="list">
+            <img
+              alt="Rick Sanchez"
+              src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            />
+            <div className="bk container">
+              <h4 className="bk name">Rick Sanchez</h4>
+              <p className="bk species">Human</p>
+            </div>
+          </li> */}
+        </ul>
+      </section>
     </>
   );
 }
