fbad9acd126242ff91361830fecaf908fefcb7ae
saramartara <saramartinezaragon@gmail.com>
2021-03-05 08:55:16 +0100

    fix origin props at CharacterDetail

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -6,21 +6,22 @@ const CharacterDetail = (props) => {
   const { name, status, species, origin, image, episode } = props.character;
   return (
     <div className="detailWrapper">
-      <Link className="detailLink" to="/">Volver</Link>
+      <Link className="detailLink" to="/">
+        Volver
+      </Link>
       <article className="detailCard">
-        <img
-          className="detailCard__img"
-          title={name}
-          alt={name}
-          src={image}
-        />
+        <img className="detailCard__img" title={name} alt={name} src={image} />
         <section className="detailCard__sec">
           <h4 className="detail__name">{name}</h4>
           <ul className="detail__list">
-            <li className="detail__item">Status: <span>{status}</span></li>
-            <li className="detail__item">Species: <span>{species}</span></li>
             <li className="detail__item">
-              Origin: <span>{origin.name}</span>
+              Status: <span>{status}</span>
+            </li>
+            <li className="detail__item">
+              Species: <span>{species}</span>
+            </li>
+            <li className="detail__item">
+              Origin: <span>{origin}</span>
             </li>
             <li className="detail__item">
               Episodes: <span>{episode.length}</span>
@@ -30,12 +31,9 @@ const CharacterDetail = (props) => {
       </article>
     </div>
   );
-
 };
 
-
-
-CharacterDetail.propTypes ={
+CharacterDetail.propTypes = {
   name: PropTypes.string,
   id: PropTypes.number,
   character: PropTypes.object,
