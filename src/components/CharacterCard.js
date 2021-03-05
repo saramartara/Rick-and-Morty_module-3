e5b75b50ae20d367061d8bc178f2e3cd727f2795
saramartara <saramartinezaragon@gmail.com>
2021-03-05 14:39:07 +0100

    add icons styles

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,37 +2,44 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-
 const CharacterCard = (props) => {
-    const { name, species, image, status } = props.character;
+  const { name, species, image, status } = props.character;
 
-// Status and species ICONS: conditional rendering
+  // Status and species ICONS: conditional rendering
 
-    const deadStatus = () => status === "Dead" ? <i class="fa fa-hackaday" aria-hidden="true"></i> : "";
+  const deadStatus = () =>
+    status === 'Dead' ? (
+      <i title={status} className="fa fa-hackaday" aria-hidden="true"></i>
+    ) : (
+      ''
+    );
 
-    const speciesIcon = () => species === "Alien" ? <i class="fa fa-reddit-alien" aria-hidden="true"></i> : <i class="fa fa-universal-access" aria-hidden="true"></i> ;
-
-
+  const speciesIcon = () =>
+    species === 'Alien' ? (
+      <i title={species} className="fa fa-reddit-alien"></i>
+    ) : (
+      <i title={species} className="fa fa-universal-access"></i>
+    );
 
   return (
     <Link className="card" to={`/character/${props.character.id}`}>
+      <img
+        className="card__img"
+        title={`click to see ${name} details`}
+        alt={name}
+        src={image}
+      />
+      <div className="card__container">
+        <h4 className="card__name">{name}</h4>
+             
+        <span>{speciesIcon()}</span> <span>{deadStatus()}</span>
       
-        <img className="card__img"
-          title={`click to see ${name} details`}
-          alt={name}
-          src={image}
-        />
-        <div className="card__container">
-          <h4 className="card__name">{name}</h4>
-          <p className="card__species">{species}</p>
-          <p className="card__icon">{speciesIcon()}</p>
-          <p className="card__icon">{deadStatus()}</p>
-        </div>
+      </div>
     </Link>
   );
-}
+};
 
-CharacterCard.propTypes ={
+CharacterCard.propTypes = {
   name: PropTypes.string,
   id: PropTypes.number,
   character: PropTypes.object,
